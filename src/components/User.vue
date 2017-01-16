48a1303cc27f25441d906706cdd77269dca9b91d
Christian Nascimento <christian.nascimento@resultadosdigitais.com.br>
2017-01-16 07:40:18 -0200

    Add navbar and user

---
+++ src/components/User.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="ui card centered">
+    <div class="image">
+      <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50">
+    </div>
+    <div class="content">
+      <a class="header">{{ user.name }}</a>
+      <div class="meta">
+        <span class="date">{{ user.email }}</span>
+      </div>
+      <div class="description">
+        {{ user.company.bs }} at {{ user.company.name }}
+      </div>
+    </div>
+    <div class="extra content">
+      <a>
+        <i class="user icon"></i>
+        22 Friends
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Gravatar from './util/Gravatar'
+const ENDPOINT = 'https://jsonplaceholder.typicode.com'
+
+export default {
+  name: 'user',
+  components: {
+    Gravatar
+  },
+  data () {
+    return {
+      user: []
+    }
+  },
+
+  mounted () {
+    axios.get(ENDPOINT + '/users?id=' + this.$route.params.id).then((response) => {
+      this.$set(this, 'user', response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  margin: 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+article {
+  border: 1px solid #ddd;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 4px;
+}
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="ui middle aligned divided list">
+    <div class="item" v-for="user in users">
+      <div class="right floated content">
+        <router-link class="ui button" :to="{ name: 'user', params: { id: user.id }}">Edit</router-link>
+      </div>
+      <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50" alt="avatar" class="ui avatar image">
+      <div class="content">
+        {{ user.name }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import Gravatar from './util/Gravatar'
+
+const ENDPOINT = 'https://jsonplaceholder.typicode.com'
+
+export default {
+  name: 'users',
+  components: {
+    Gravatar
+  },
+  data () {
+    return {
+      users: []
+    }
+  },
+  mounted () {
+    axios.get(ENDPOINT + '/users').then((response) => {
+      this.$set(this, 'users', response.data)
+    }).catch((error) => {
+      console.log(error)
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+  margin: 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+article {
+  border: 1px solid #ddd;
+  margin-bottom: 10px;
+  padding: 10px;
+  border-radius: 4px;
+}
+</style>
