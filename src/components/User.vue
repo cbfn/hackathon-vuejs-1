e37405bb27fd32a671f3ede9738cdd0ab89997ab
Christian Nascimento <christian.nascimento@resultadosdigitais.com.br>
2017-01-17 00:40:42 -0200

    change style

--- src/components/User.vue
+++ src/components/User.vue
@@ -38,29 +38,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-  margin: 0;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-article {
-  border: 1px solid #ddd;
-  margin-bottom: 10px;
-  padding: 10px;
-  border-radius: 4px;
-}
 </style>
